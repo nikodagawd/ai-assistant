521d1ebb3cee4f3eee64db770bded4fb6ef07732
Nicole Molina <nicole.andrea.molina.c@gmail.com>
2026-01-22 10:17:09 +0100

    Added History Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     root to: "pages#home"
     post "/", to: "pages#home"
     get "up" => "rails/health#show", as: :rails_health_check
-    resources :chats, only: [:show]
+    resources :chats, only: [:show, :index]
+
   end
 end
