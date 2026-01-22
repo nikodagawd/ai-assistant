9fd8134155db96a58e879b9bd7e90339f95b8363
Nicole Molina <nicole.andrea.molina.c@gmail.com>
2026-01-22 09:57:50 +0100

    Fixed view

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,11 @@
+
 class ChatsController < ApplicationController
   def show
     @chat = Chat.find(params[:id])
+  end
+
+
+  def index
+    @chats = current_user.chats.order(created_at: :desc)
   end
 end
